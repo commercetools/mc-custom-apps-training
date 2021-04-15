eaa5fd03ae4c93c84e2134e479dd829245099a33
Fady Fawzy <fady.fawzy19@hotmail.com>
2021-04-15 14:04:30 +0200

    fixing the external server call ++ removing redux

--- src/components/ExternalServer/ExternalServer.js
+++ src/components/ExternalServer/ExternalServer.js
@@ -5,69 +5,50 @@ import { useShowApiErrorNotification } from "@commercetools-frontend/actions-glo
 import PrimaryButton from "@commercetools-uikit/primary-button";
 import { ContentNotification, Spacings } from "@commercetools-frontend/ui-kit";
 
-const externalApiUrl = "https://external-server.now.sh/sayhi";
-const initialState = {
-  isLoading: false,
-  data: null,
-  error: null,
-};
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "ok":
-      return { isLoading: false, data: action.payload, error: null };
-    case "error":
-      return { isLoading: false, data: null, error: action.payload };
-    default:
-      return state;
-  }
-};
+const externalApiUrl =
+  "https://state-machines-gcp-eu-commercetools-playground.vercel.app/api/echo";
+
 const ExternalServer = () => {
   // The asyncDispatch is a wrapper around the redux dispatch and provides
   // the correct return type definitions because the action resolves to a Promise.
   const asyncDispatch = useAsyncDispatch();
   const showApiErrorNotification = useShowApiErrorNotification();
-  const [state, dispatch] = useReducer(reducer, initialState);
   const [type, setType] = useState("info");
+  const [error, setError] = useState(null);
   const [message, setMessage] = useState(
     "Click on the button above to send a call to the External Server"
   );
-  const [from, setFrom] = useState("");
-  
 
-  const handleClick = () =>{
+  const handleClick = () => {
     asyncDispatch(actions.forwardTo.get({ uri: externalApiUrl }))
       .then((result) => {
-        setType("success")
-        setMessage(result.message)
-        setFrom(result.from)
-        dispatch({ type: "ok", payload: result });
+        setType("success");
+        setMessage(result.message);
       })
       .catch((error) => {
-        dispatch({ type: "error", payload: error });
+        setType("error");
+        setError(error.message);
         showApiErrorNotification({ errors: error });
       });
-  }
- 
-  if (state.isLoading) return "Loading...";
-  if (state.error) return (
-    <ContentNotification type="error">
+  };
+
+  if (error)
+    return (
+      <ContentNotification type="error">
         {" "}
-        {`Message: ${state.error.message}`}{" "}
+        {`Message: ${error}`}{" "}
       </ContentNotification>
-  );
-  
+    );
+
   return (
     <Spacings.Stack scale="l" alignItems="flex-start">
       <Text.Headline as="h3">External fetch example</Text.Headline>
 
-      <PrimaryButton
-        label="Call to External Server"
-        onClick={handleClick}
-      />
+      <PrimaryButton label="Call to External Server" onClick={handleClick} />
 
       <ContentNotification type={type}>
         {" "}
-        {`Message: ${message} ${from}`}{" "}
+        {`Message: ${message}`}{" "}
       </ContentNotification>
     </Spacings.Stack>
   );
