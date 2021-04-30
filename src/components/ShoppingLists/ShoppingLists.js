86691bb833a908ec40dc9aac8c42dc7112b05361
Fady Fawzy <fady.fawzy19@hotmail.com>
2021-04-30 11:28:57 +0200

    added updateMutation

--- src/components/ShoppingLists/ShoppingLists.js
+++ src/components/ShoppingLists/ShoppingLists.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from "react";
 import Text from "@commercetools-uikit/text";
 import { GRAPHQL_TARGETS } from "@commercetools-frontend/constants";
 import { fetchShoppingLists } from "./queries.graphql";
-import { createShoppingList, deleteShoppingList } from "./mutations.graphql";
+import {
+  createShoppingList,
+  deleteShoppingList,
+  updateShoppingListName,
+} from "./mutations.graphql";
 import DataTableManager from "@commercetools-uikit/data-table-manager";
 import DataTable from "@commercetools-uikit/data-table";
 import PrimaryButton from "@commercetools-uikit/primary-button";
 import { FormModalPage } from "@commercetools-frontend/application-components";
 import Spacings from "@commercetools-uikit/spacings";
 import TextField from "@commercetools-uikit/text-field";
+import TextInput from "@commercetools-uikit/text-input";
 
 import {
   useShowApiErrorNotification,
@@ -20,27 +25,34 @@ import { useFormik } from "formik";
 import { useQuery, useMutation } from "@apollo/client/react";
 
 import IconButton from "@commercetools-uikit/icon-button";
-import { CheckInactiveIcon, EditIcon } from "@commercetools-uikit/icons";
+import {
+  CheckInactiveIcon,
+  EditIcon,
+  CheckBoldIcon,
+} from "@commercetools-uikit/icons";
 
 const target = GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM;
 
 const ShoppingLists = () => {
   const [formVisibility, setFormVisibility] = useState(false);
+  const [updateVisibility, setUpdateVisibility] = useState({
+    status: false,
+    rowId: "",
+    version: "",
+    name: "",
+  });
   const showApiErrorNotification = useShowApiErrorNotification();
   const showSuccessNotification = useShowNotification();
   const { error, data, loading } = useQuery(fetchShoppingLists, {
     context: { target },
   });
   const refetch = {
-    // Then re-run 
-    refetchQueries: [
-      { query: fetchShoppingLists ,
-        context:{target}
-      }
-    ]
-  }
-  const [saveShoppingList] = useMutation(createShoppingList,refetch);
-  const [delShoppingList] = useMutation(deleteShoppingList,refetch);
+    // Then re-run
+    refetchQueries: [{ query: fetchShoppingLists, context: { target } }],
+  };
+  const [saveShoppingList] = useMutation(createShoppingList, refetch);
+  const [updateShoppingList] = useMutation(updateShoppingListName, refetch);
+  const [delShoppingList] = useMutation(deleteShoppingList, refetch);
   const cols = [
     {
       key: "id",
@@ -55,8 +67,22 @@ const ShoppingLists = () => {
     {
       key: "name",
       label: "Name",
-      renderItem: (row) =>
-        row?.nameAllLocales ? row?.nameAllLocales[0]?.value : "",
+      renderItem: (row) => {
+        if (updateVisibility.status && updateVisibility.rowId == row.id)
+          return (
+            <TextInput
+              value={updateVisibility.name}
+              onChange={(event) =>
+                setUpdateVisibility({
+                  ...updateVisibility,
+                  name: event.target.value,
+                })
+              }
+            />
+          );
+
+        return row?.nameAllLocales ? row?.nameAllLocales[0]?.value : "";
+      },
     },
     {
       key: "actions",
@@ -64,26 +90,76 @@ const ShoppingLists = () => {
       renderItem: (row) => {
         const version = row.version;
         const id = row.id;
+        const name = row?.nameAllLocales[0]?.value;
+        const locale = row?.nameAllLocales[0]?.locale;
         return (
-          <IconButton
-            icon={<CheckInactiveIcon />}
-            label="Delete Record"
-            onClick={async (row) => {
-              const { error } = await delShoppingList({
-                variables: {
-                  version,
-                  id,
-                },
-                context: { target },
-              });
-              if (error) showApiErrorNotification({ error: error });             
-              showSuccessNotification({
-                kind: "success",
-                domain: DOMAINS.SIDE,
-                text: "Shopping List Deleted Successfully",
-              });
-            }}
-          />
+          <React.Fragment>
+            <IconButton
+              icon={<CheckInactiveIcon />}
+              label="Delete Record"
+              onClick={async (row) => {
+                const { error } = await delShoppingList({
+                  variables: {
+                    version,
+                    id,
+                  },
+                  context: { target },
+                });
+                if (error) showApiErrorNotification({ error: error });
+                showSuccessNotification({
+                  kind: "success",
+                  domain: DOMAINS.SIDE,
+                  text: "Shopping List Deleted Successfully",
+                });
+              }}
+            />
+            {updateVisibility.status ? (
+              updateVisibility.rowId == id ? (
+                <IconButton
+                  icon={<CheckBoldIcon />}
+                  label="Submit Edit Record"
+                  onClick={async (row) => {
+                    const { error } = await updateShoppingList({
+                      variables: {
+                        version,
+                        id,
+                        locale,
+                        value: updateVisibility.name,
+                      },
+                      context: { target },
+                    });
+                    if (error) showApiErrorNotification({ error: error });
+                    showSuccessNotification({
+                      kind: "success",
+                      domain: DOMAINS.SIDE,
+                      text: "Shopping List updated Successfully",
+                    });
+                    setUpdateVisibility({
+                      status: false,
+                      rowId: "",
+                      version: "",
+                      name: "",
+                    });
+                  }}
+                />
+              ) : (
+                ""
+              )
+            ) : (
+              <IconButton
+                icon={<EditIcon />}
+                label="Edit Record"
+                onClick={(row) =>
+                  setUpdateVisibility({
+                    status: true,
+                    rowId: id,
+                    version,
+                    name,
+                  })
+                }
+              />
+            )}
+          </React.Fragment>
         );
       },
     },
@@ -101,8 +177,6 @@ const ShoppingLists = () => {
     validateOnBlur: false,
   });
 
-  
-
   const handleSubmit = async (formValues) => {
     const { error } = await saveShoppingList({
       variables: {
@@ -119,7 +193,6 @@ const ShoppingLists = () => {
       text: "Shopping List added Successfully",
     });
   };
-
 
   if (loading) return "Loading...";
   if (error) return `---Error! ${error.message}`;
